ff77d4e13ff8ff7517ec582825a4f23d9c1d2ad5
Jan Theon <vonhara@gmail.com>
2025-10-16 12:48:24 +0300

    Fix formatting in app.routes.ts for consistent indentation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'feedback', component: FeedbackComponent },
-    { path: '**', component: PagenotfoundComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '**', component: PagenotfoundComponent },
 ];
