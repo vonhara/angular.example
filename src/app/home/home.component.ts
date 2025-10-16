629f57c5ee631a331f74f2687d3fba8c08acc64c
Jan Theon <vonhara@gmail.com>
2025-10-16 12:49:01 +0300

    Fix formatting in HomeComponent for consistent code style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
@@ -6,20 +6,16 @@ import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    MatButtonModule,
-    MatListModule,
-  ],
+  imports: [MatButtonModule, MatListModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   headerText = 'Testing Angular Applications';
 
-  constructor(public router: Router) { }
+  constructor(public router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showFeedbackPage(): void {
     this.router.navigate(['feedback']);
