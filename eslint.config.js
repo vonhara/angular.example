d0ab23bc168540997e854fedf66949990c950eb8
Jan Theon <vonhara@gmail.com>
2025-10-16 12:48:07 +0300

    Add Prettier integration to ESLint config for Angular example project

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const prettier = require("eslint-config-prettier");
+const prettierPlugin = require("eslint-plugin-prettier");
 
 module.exports = tseslint.config(
   {
@@ -11,23 +13,29 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      prettier,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     processor: angular.processInlineTemplates,
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
+        { type: "attribute", prefix: "app", style: "camelCase" },
       ],
       "@angular-eslint/component-selector": [
         "error",
+        { type: "element", prefix: "app", style: "kebab-case" },
+      ],
+      "prettier/prettier": [
+        "error",
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          singleQuote: true,
+          semi: true,
+          printWidth: 100,
+          trailingComma: "es5",
+          bracketSpacing: true,
         },
       ],
     },
